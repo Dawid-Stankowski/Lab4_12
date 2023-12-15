3a1dd299bee302831291d8619b21c09ecc839e0d
Dawid-Stankowski <dawid441@interia.pl>
2023-12-15 07:58:12 +0100

    1.0

--- src/myapp/MyCharacter.java
+++ src/myapp/MyCharacter.java
@@ -1,2 +1,46 @@
-package myapp;public class MyCharacter {
+package myapp;
+
+public class MyCharacter {
+    private String imie;
+    private String klasaPostaci;
+    private String rasa;
+    private int sila;
+    public MyCharacter(String imie, String klasaPostaci, String rasa, int sila) {
+        this.imie = imie;
+        this.klasaPostaci = klasaPostaci;
+        this.rasa = rasa;
+        this.sila = sila;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getKlasaPostaci() {
+        return klasaPostaci;
+    }
+
+    public void setKlasaPostaci(String klasaPostaci) {
+        this.klasaPostaci = klasaPostaci;
+    }
+
+    public String getRasa() {
+        return rasa;
+    }
+
+    public void setRasa(String rasa) {
+        this.rasa = rasa;
+    }
+
+    public int getSila() {
+        return sila;
+    }
+
+    public void setSila(int sila) {
+        this.sila = sila;
+    }
 }
--- src/testowanie/TestCharacter.java
+++ src/testowanie/TestCharacter.java
@@ -1,2 +1,15 @@
-package testowanie;public class TestCharacter {
+package testowanie;
+import myapp.MyCharacter;
+
+public class TestCharacter {
+    public static void main(String[] args) {
+
+        MyCharacter myCharacter = new MyCharacter("Kamil", "Wojownik", "ludzi", 500);
+
+
+        System.out.println("Imię: " + myCharacter.getImie());
+        System.out.println("Klasa postaci: " + myCharacter.getKlasaPostaci());
+        System.out.println("Rasa: " + myCharacter.getRasa());
+        System.out.println("Siła: " + myCharacter.getSila());
+    }
 }
